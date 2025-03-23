4f2caac5c1f1c1ddbe0926551db239e401e95dc8
Carlos Mendez <carlosmendez777@gmail.com>
2025-03-23 21:32:08 +0900

    asd

--- esda101.py
+++ esda101.py
@@ -1,18 +1,20 @@
-  import geopandas as gpd
-  import plotly.express as px
-
-  # Load data
-  data = gpd.read_file('https://github.com/quarcs-lab/project2021o-notebook/raw/main/map_and_data.geojson')
-  data = data.to_crs(epsg=4326)
-
-  # Ensure a unique ID column
-  data["id"] = data.index.astype(str)
-
-  # Convert GeoDataFrame to GeoJSON dictionary
-  geojson_dict = data.__geo_interface__
-
-  # Plot with proper geojson and explicit color scale "viridis"
-  fig = px.choropleth_mapbox(
+#%% Import libraries
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+  
+#%% Load data
+data = gpd.read_file('https://github.com/quarcs-lab/project2021o-notebook/raw/main/map_and_data.geojson')
+data = data.to_crs(epsg=4326)
+  
+#%% Ensure a unique ID column
+data["id"] = data.index.astype(str)
+  
+#%% Convert GeoDataFrame to GeoJSON dictionary
+geojson_dict = data.__geo_interface__
+  
+#%% Plot with proper geojson and explicit color scale "viridis"
+fig = px.choropleth_mapbox(
       data_frame=data,
       geojson=geojson_dict,
       locations="id",
@@ -26,6 +28,7 @@
       opacity=0.6
   )
 
-  # Set figure dimensions to 600x600 pixels (6 in x 6 in at 100 DPI)
-  fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, width=630, height=600)
-  fig.show()
+# Set figure dimensions to 600x600 pixels (6 in x 6 in at 100 DPI)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, width=630, height=600)
+fig.show()
+
